1fb5d7d500229569f3e66604322634aee6bbfa1c
cris959 <christiangaray959@gmail.com>
2026-07-22 20:52:41 -0300

    feat: establecer estructura de capas para unir el modulo de data

--- backend/src/main/java/saludfinanciera/finanzas/FinanzasApplication.java
+++ backend/src/main/java/saludfinanciera/finanzas/FinanzasApplication.java
@@ -1,4 +1,4 @@
-package saludFinanciera.finanzas;
+package saludfinanciera.finanzas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
